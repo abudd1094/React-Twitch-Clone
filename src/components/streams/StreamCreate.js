e5ea9b6ef444a9bf51bf9a50d4f3c81af5963f5e
Alec Budd <abudd1094@gmail.com>
2019-04-15 18:56:05 +0200

    StreamCreate component updated with redux-form and fluid error handling

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-  renderInput({ input, label }) { // takes formProps key/value pairs and adds them as properties to the input element, we destructure input out when passing in formProps, before it would be ...formProps.input
+  renderError({ error, touched }) { // destructuring error and touched keys out of the redux-form meta object
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+  
+  renderInput = ({ input, label, meta }) => { // takes formProps key/value pairs and adds them as properties to the input element, we destructure input out when passing in formProps, before it would be ...formProps.input
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}` // creating some logic to conditionally add the className error to our div, which in Semantic UI makes the input field red when there is an error
     return(
-      <div>
+      <div className={className}>
         <label htmlFor="">{label}</label>      {/* we add our own custom prop 'label' and destructure it */}
-        <input {...input} /> 
+        <input {...input} autoComplete="off" /> 
+        <div>{this.renderError(meta)}</div>   {/* we pass meta object from redux form into the renderError helper method */}
       </div>
       
     )  
@@ -19,7 +31,10 @@ class StreamCreate extends React.Component {
   render() {
     // console.log(this.props)
     return( // Field element renders a React component or runs a function that returns JSX
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+      <form 
+        onSubmit={this.props.handleSubmit(this.onSubmit)} 
+        className="ui form error"
+      >
         <Field name="title" component={this.renderInput} label="enter title"/> 
         <Field name="description" component={this.renderInput} label="enter description"/>
         <button className="ui button primary">Submit</button>
