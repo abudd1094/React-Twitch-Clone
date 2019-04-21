462afd3b72fe1b1f7cebcdd103e452625db55bcd
Alec Budd <abudd1094@gmail.com>
2019-04-21 14:09:43 +0200

    streaming implemented

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
   }
   
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer(); // if component fetches the stream successfully in the future, re build player
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+  
+  buildPlayer() {
+    if (this.player || !this.props.stream) { // if we already have a player or the stream does not yet exist
+      return;
+    }
+
+    const { id } = this.props.match.params;
+    this.player = flv.createPlayer({ // configuring video streaming server
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>
@@ -14,8 +46,9 @@ class StreamShow extends React.Component {
     
     const { title, description } = this.props.stream;  // destructuring title and description properties from the stream
     
-    return(
+    return( // for controls={true} in the HTML video player we can simply write controls, it will be read the same
       <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls={true} /> 
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
